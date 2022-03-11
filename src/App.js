884a6924bb88071fc1bb02d390fb4e9830a69406
chandrahk <55194898+chandrahk@users.noreply.github.com>
2022-03-11 14:26:52 -0600

    Changed infinite loop protection and added delay function

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ const options = [
   { value: "en", label: "English" }
 ];
 
+function syncDelay(milliseconds) {
+  var start = new Date().getTime();
+  var end = 0;
+  while (end - start < milliseconds) {
+    end = new Date().getTime();
+  }
+}
+
 function App() {
   const defaultLanguage = 0;
   const [selected, setSelected] = useState(options[defaultLanguage].value);
